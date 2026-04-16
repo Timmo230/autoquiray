5a28cf4dfb1375945992ab73f33846b4fd0800e3
Matias <mquiela830@g.educaand.es>
2026-04-16 23:32:22 +0200

    Sync remaining local project changes

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -5,6 +5,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link rel="stylesheet" href="/resources/css/main.min.css">
-<link rel="stylesheet" href="/resources/css/general.css">
-<link rel="icon" href="\resources\img\logo\favicon.png">
+<link rel="stylesheet" href="{{ asset('resources/css/main.min.css') }}">
+<link rel="stylesheet" href="{{ asset('resources/css/general.css') }}">
+<link rel="icon" href="{{ asset('resources/img/logo/favicon.png') }}">
